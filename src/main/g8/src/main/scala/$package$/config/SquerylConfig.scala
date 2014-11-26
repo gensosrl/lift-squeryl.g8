63f811c1c84490ac594a7d95f13e2790ad92a508
Juan Jose Olivera <juan@genso.com.bo>
2014-11-26 10:57:00 -0400

    changed Schema class name

--- src/main/g8/src/main/scala/$package$/config/SquerylConfig.scala
+++ src/main/g8/src/main/scala/$package$/config/SquerylConfig.scala
@@ -15,13 +15,15 @@ import model._
 
 object SquerylConfig extends Factory with Loggable {
 
+  lazy val databaseProvider = "$database_provider"
+
   private def initH2(schema: () => Schema*) {
     Class.forName("org.h2.Driver")     
     import org.squeryl.adapters.H2Adapter
     import net.liftweb.squerylrecord.SquerylRecord
     import org.squeryl.Session
     SquerylRecord.initWithSquerylSession(Session.create(
-      DriverManager.getConnection("jdbc:h2:mem:dbname;DB_CLOSE_DELAY=-1", "sa", ""),
+      DriverManager.getConnection(Props.get("db.url", "jdbc:h2:mem:dbname;DB_CLOSE_DELAY=-1"), Props.get("db.user", "$db_user"), Props.get("db.password", "$db_password")),
       new H2Adapter))    
     inTransaction {
       try {
@@ -36,8 +38,59 @@ object SquerylConfig extends Factory with Loggable {
       })
   }
 
+  private def initMysql(schema: () => Schema*) {
+    Class.forName("com.mysql.jdbc.Driver")     
+    import org.squeryl.adapters.MySQLInnoDBAdapter
+    import net.liftweb.squerylrecord.SquerylRecord
+    import org.squeryl.Session
+    SquerylRecord.initWithSquerylSession(Session.create(
+      DriverManager.getConnection(Props.get("db.url", "jdbc:mysql://$db_host/$db_name"), Props.get("db.user", "$db_user"), Props.get("db.password", "$db_password")),
+      new MySQLInnoDBAdapter))    
+    inTransaction {
+      try {
+        schema.map(s => s().create)
+      } catch {
+        case e: Throwable => e.printStackTrace()
+        throw e;
+      }
+    }
+    LiftRules.liftRequest.append({
+        case Req("console" ::_, _, _) => false
+      })
+  }
+
+  private def initPostgreSql(schema: () => Schema*) {
+    Class.forName("org.postgresql.Driver")    
+    import org.squeryl.adapters.PostgreSqlAdapter
+    import net.liftweb.squerylrecord.SquerylRecord
+    import org.squeryl.Session
+    SquerylRecord.initWithSquerylSession(Session.create(
+      DriverManager.getConnection(Props.get("db.url", "jdbc:postgresql://$db_host/$db_name"), Props.get("db.user", "$db_user"), Props.get("db.password", "$db_password")),
+      new PostgreSqlAdapter))    
+    inTransaction {
+      try {
+        schema.map(s => s().create)
+      } catch {
+        case e: Throwable => e.printStackTrace()
+        throw e;
+      }
+    }
+    LiftRules.liftRequest.append({
+        case Req("console" ::_, _, _) => false
+      })
+  }
+
+
+
   def init = {
-    initH2(() => DbSchema, () => UserSchema)    
+    databaseProvider match {
+      case "h2" =>
+        initH2(() => SquerylAuthSchema, () => DbSchema)    
+      case "mysql" =>
+        initMysql(() => SquerylAuthSchema, () => DbSchema)    
+      case "postgresql" =>
+        initPostgreSql(() => SquerylAuthchema, () => DbSchema)    
+    }
   }
 }
 
